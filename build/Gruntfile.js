a823edbcdb244afdfe7f67ad4f57aabb3ab870f6
Outsider <outsideris@gmail.com>
2015-01-17 20:39:13 +0900

    add stylus compile enviroment

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+  require('load-grunt-tasks')(grunt);
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -7,9 +9,20 @@ module.exports = function(grunt) {
       version: "0.15.9",
       outputDir: "./atom-shell",
       rebuild: true
+    },
+    stylus: {
+      compile: {
+        files: {
+          '../app/css/style.css': ['../app/css/style.styl']
+        }
+      }
+    },
+    watch: {
+      stylus: {
+        files: ['../app/css/**/*.styl'],
+        tasks: ['stylus:compile']
+      }
     }
   });
-
-  grunt.loadNpmTasks('grunt-download-atom-shell');
 
 };
