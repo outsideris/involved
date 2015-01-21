1da1376cf6bbbaecc2d4cc975a070681699ef214
Outsider <outsideris@gmail.com>
2015-01-21 23:45:25 +0900

    add github service and user profile

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,29 @@
 (function() {
   'use strict';
 
-  angular.module('involved', []);
+  angular.module('involved', ['ngResource'])
+    .run(function($rootScope, github) {
+      github.user().get(function(user, getResponseHeaders) {
+        $rootScope.me = user;
+      });
+    });
 
   // controllers
   angular.module('involved')
-    .controller('TimelineCtrl', function($scope) {
+    .controller('TimelineCtrl', function($scope, github) {
 
     });
 
   // services
+  angular.module('involved')
+    .factory('github', function($resource) {
+      var token = 'b4e796e89482217563b3f85b687039c6d7b78c08';
+      return {
+        user: function () {
+          return $resource('https://api.github.com/user', {
+            access_token: token
+          });
+        }
+      };
+    });
 })();
