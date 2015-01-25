71b247e9bb51e43b7f663e299d1ef4193eb53d67
Outsider <outsideris@gmail.com>
2015-01-26 02:39:42 +0900

    extract github host into variable

--- app/js/app.js
+++ app/js/app.js
@@ -17,10 +17,11 @@
   // services
   angular.module('involved')
     .factory('github', function($resource) {
+      var githubHost = 'https://api.github.com';
       var token = 'b4e796e89482217563b3f85b687039c6d7b78c08';
       return {
-        user: function () {
-          return $resource('https://api.github.com/user', {
+        user: function() {
+          return $resource(githubHost + '/user', {
             access_token: token
           });
         }
