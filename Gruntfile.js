a8c65825b23b4df99822934ab3717a6f0c31f5bb
Outsider <outsideris@gmail.com>
2015-06-03 18:12:55 +0900

    bump up electron

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     "download-electron": {
-      version: "0.25.2",
+      version: "0.27.2",
       outputDir: "./build/electron",
       rebuild: true
     },
