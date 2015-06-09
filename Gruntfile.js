0d2db77c2fc2df3eb64643deccccf8cb9987a082
Outsider <outsideris@gmail.com>
2015-06-09 12:42:02 +0900

    bump up Electron to fix dev mode bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     "download-electron": {
-      version: "0.27.2",
+      version: "0.27.3",
       outputDir: "./build/electron",
       rebuild: true
     },
