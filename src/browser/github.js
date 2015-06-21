51aaeae2e2304ec04aa403cad7a9cdaca6b9c73e
Outsider <outsideris@gmail.com>
2015-06-21 23:38:18 +0900

    extract methods

--- src/browser/github.js
+++ src/browser/github.js
@@ -19,11 +19,21 @@ var req = function(options) {
 };
 
 module.exports = (function() {
-  var origin = 'https://api.github.com',
-      TOKEN = '';
+  var TOKEN = '';
 
-  // user-agent required
-  // https://developer.github.com/v3/#user-agent-required
+  var url = function(path) {
+    return 'https://api.github.com' + path;
+  };
+
+  var headers = function() {
+    // user-agent required
+    // https://developer.github.com/v3/#user-agent-required
+    return {
+      'User-Agent': 'Involved-App',
+      'Authorization': 'token ' + TOKEN
+    };
+  };
+
   return {
     pageSize: 60,
     setToken: function(t) {
@@ -32,29 +42,20 @@ module.exports = (function() {
     },
     me: function() {
       return req({
-        url: origin + '/user',
-        headers: {
-          'User-Agent': 'Involved-App',
-          'Authorization': 'token ' + TOKEN
-        }
+        url: url('/user'),
+        headers: headers()
       });
     },
     user: function(username) {
       return req({
-        url: origin+'/users/'+username,
-        headers: {
-          'User-Agent': 'Involved-App',
-          'Authorization': 'token ' + TOKEN
-        }
+        url: url('/users/'+username),
+        headers: headers()
       });
     },
     repoEvents: function(owner, repo, page) {
       return req({
-        url: origin+'/repos/'+owner+'/'+repo+'/events',
-        headers: {
-          'User-Agent': 'Involved-App',
-          'Authorization': 'token ' + TOKEN
-        },
+        url: url('/repos/'+owner+'/'+repo+'/events'),
+        headers: headers(),
         qs: {
           page: page || 1,
           'per_page': this.pageSize
@@ -63,11 +64,8 @@ module.exports = (function() {
     },
     emojis: function() {
       return req({
-        url: origin+'/emojis',
-        headers: {
-          'User-Agent': 'Involved-App',
-          'Authorization': 'token ' + TOKEN
-        }
+        url: url('/emojis'),
+        headers: headers()
       });
     }
   };
